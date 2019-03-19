c0438d508f1ed973735ab4cd199298847c5eca07
David Hodges <david.william.hodges@gmail.com>
2019-03-19 12:14:15 -0400

    Added v1 of example 1

---
+++ example 1/servernamepull.ps1
@@ -0,0 +1,36 @@
+﻿param ( [string]$servername,
+        [string]$outputfile,
+        [switch]$silent)
+
+function Log-Info 
+{
+    param ([string]$output)
+    if(!$silent)
+    {
+        Write-Host $output
+    }
+}
+#check if input call was made properly
+if(!$servername)
+{
+    Log-Info -output "Please call script in the format: .\servernamepull.ps1 -servername <yourservername>"
+    
+    return $false
+}
+#Get Execution Path (for default pathing)
+
+#check output file parameter
+if(!$outputfile)
+{
+    Log-Info -output "using default outputfile location: \servernameresults.csv"
+    
+    $outputfile = 'servernameresult.csv'
+}
+#if the file doesn't exist, create it
+if(!(Test-Path $outputfile))
+{
+    Log-Info -output "outputfile does not exist. creating with headers."
+    $headers = "servername,IP"
+    Set-Content -Path $outputfile -Value $headers
+}
+return $true
